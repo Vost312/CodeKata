dfc060270e36f57e6d55ab27e610b91a8a527b26
VOST <victoroscarsato@hotmail.com>
2018-05-07 15:43:21 -0500

    CodeKata

---
+++ CodeKata/src/codekata/CodeKata.java
@@ -0,0 +1,13 @@
+package codekata;
+
+/**
+ * @author VosT
+ */
+public class CodeKata {
+
+    public static void main(String[] args) {
+        Solucion S = new Solucion();
+        String salida = S.lectura(745);
+        System.out.println(salida);
+    }   
+}
